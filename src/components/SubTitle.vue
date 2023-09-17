3de6a3e5afa74bf674f1b3e429175287200fc93d
[lucie simatovic] <[simatovic.lucie@gmail.com]>
2023-09-17 19:28:56 +0200

    initiate subtitles component

---
+++ src/components/SubTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="subtitle">
+    <slot name="message"></slot>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+}
+.subtitle {
+  font-family: 'Mochiy Pop One', sans-serif;
+  font-size: 2rem;
+  text-shadow: black 1px 1px 1px;
+  color: yellow;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #ffffff00;
+  padding: 10px 0;
+  text-align: center;
+  z-index: 1000;
+}
+</style>
